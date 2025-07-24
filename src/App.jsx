29b3e591088fdc874c3f93106cfa820db368ee36
Artur Babii <artur.babii.088@gmail.com>
2025-07-24 22:14:31 +0300

    feat: initial pokemon-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import Filter from "./components/Filter/Filter";
 import Header from "./components/Header/Header";
@@ -105,8 +104,3 @@ function App() {
 }
 
 export default App;
-=======
-export default function App() {
-  return <h1>Hello Pokedex!</h1>;
-}
->>>>>>> da597b176cc08e8edfb69bc5d76c50dbd5b5d5c4
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-=======
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  plugins: [react()],
 });
->>>>>>> da597b176cc08e8edfb69bc5d76c50dbd5b5d5c4
